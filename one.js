2ad50f21772e26f430db246c55fbda4cb49364cd
emgullufsen <ericgullufsen@gmail.com>
2015-02-12 21:46:48 -0900

    see gullufsen.txt

---
+++ one.js
@@ -0,0 +1,22 @@
+// Eric Gullufsen
+
+function fresh() {
+	// make AJAX call to one.php (post) passing params indicating we want the table.
+	
+	var xm = new XMLHttpRequest();
+	
+	
+	
+}
+
+function delall() {
+	
+	// make AJAX call to one.php (post) passing param indicating a wipe of the table is to be performed
+}
+
+function checkinout() {
+	
+	// make AJAX call to one.php (post) passing params indicating video to change status for (by id?)
+	// other params?
+	
+}
--- one.php
+++ one.php
@@ -1,5 +1,24 @@
 <?php
-$name = $_POST['name'];
+if ($_POST['fresh'] == "1"){
+    buildtable();
+}
+elseif ($_POST['del'] == "1") {
+}
+
+
+function buildtable($cats=null, $add=null, $inout=null, $del=null) {
+     $conn = new mysqli('localhost', 'emg', 'gullie06', 'uno_db');
+    if($conn->connect_error){
+        
+    }
+ 
+} 
+ 
+ 
+ 
+ 
+ 
+ /*$name = $_POST['name'];
 $category = $_POST['category'];
 $length = $_POST['length'];
 
@@ -10,6 +29,8 @@ if ($conn->connect_error)
 	die('error: ' . $conn->connect_error);
 }
 
+$th = $conn->thread_id;
+
 $stmt = $conn->prepare("INSERT INTO videos (vname, category, length) VALUES (?, ?, ?)");
 if (!$stmt->bind_param("ssi", $name, $category, $length))
 {
@@ -17,7 +38,7 @@ if (!$stmt->bind_param("ssi", $name, $category, $length))
 }
 else 
 {
-	echo "Bind Succeeded";
+	echo "Bind Succeeded" . "<br>";
 }
 
 if (!$stmt->execute())
@@ -26,11 +47,12 @@ if (!$stmt->execute())
 }
 else 
 {
-	echo "insertion succeed with values: " . $name . " , " . $category . " , " . $length;
+	echo "insertion succeed with values: " . $name . " , " . $category . " , " . $length . "<br>";
 }
 
+$conn->kill($th);
+$conn->close();
 
-//comment
-$stmt->close();
+*/
 
-?>
+ 
