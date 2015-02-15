a521a930d18ebd24dcb194f8a67f2de674a03c45
emgullufsen <ericgullufsen@gmail.com>
2015-02-15 03:23:01 -0900

    yep

--- one.js
+++ one.js
@@ -1,22 +1,57 @@
 // Eric Gullufsen
 
-function fresh() {
+function fresh(num){
 	// make AJAX call to one.php (post) passing params indicating we want the table.
-	
 	var xm = new XMLHttpRequest();
+	xm.onreadystatechange = function() {
+		if (xm.readyState == 4){
+			document.getElementById("stuffs").innerHTML = xm.responseText;
+		}
+	};
+	xm.open("POST", "one.php");
+	xm.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	if (num == 1){
+		$codicil = 'callingfunction=fresh1';
+	}
+	else if (num == 2){
+		$codicil = 'callingfunction=fresh2&' + 'vname=' + document.getElementById("name").value + '&category=' + document.getElementById("category").value + '&length=' +document.getElementById("length").value; 
+	}
 	
-	
-	
+	xm.send($codicil);		
 }
 
-function delall() {
+function filter() {
+	var xm = new XMLHttpRequest();
+	xm.onreadystatechange = function() {
+		if (xm.readyState == 4){
+			document.getElementById("stuffs").innerHTML = xm.responseText;
+		}
+	};
+	xm.open("POST", "one.php");
+	xm.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	
-	// make AJAX call to one.php (post) passing param indicating a wipe of the table is to be performed
+	var f = document.getElementById("selector");
+	var g = f.options[f.selectedIndex].value; 
+	
+	$codicil = 'callingfunction=filteron&filter=' + g;
+	
+	xm.send($codicil);
 }
 
-function checkinout() {
+function delone(rowid,rented) {
+	rowid = rowid.toString();
+	rented = rented.toString();
+	var xm = new XMLHttpRequest();
+	xm.onreadystatechange = function() {
+		if (xm.readyState == 4){
+			document.getElementById("stuffs").innerHTML = xm.responseText;
+		}
+	};
 	
-	// make AJAX call to one.php (post) passing params indicating video to change status for (by id?)
-	// other params?
+	xm.open("POST", "one.php");
+	xm.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	
+	var codicil = 'callingfunction=delone&delrowid=' + rowid + '&rented=' + rented;
+	
+	xm.send(codicil);
 }
--- one.php
+++ one.php
@@ -1,58 +1,123 @@
 <?php
-if ($_POST['fresh'] == "1"){
-    buildtable();
+ini_set('display_errors', 'On');
+include 'information.php';
+
+if($_POST['callingfunction'] == 'fresh2'){
+	$mi1 = new mysqli($lh, $username, $password, $db);
+	if ($mi1->connect_error){
+		echo "noconnect" . mysqli_connect_error() . $mi1->errno;
+	}
+	
+	$th1 = $mi1->thread_id;
+	
+	$vname = $_POST['vname'];
+	$category = $_POST['category'];
+	$length = intval($_POST['length']);
+	
+	$stmt = $mi1->prepare("INSERT INTO videos (vname, category, length) VALUES (?,?,?)");
+	$stmt->bind_param("ssi", $vname, $category, $length);
+	
+	$stmt->execute();
+	$stmt->close();
 }
-elseif ($_POST['del'] == "1") {
+elseif($_POST['callingfunction'] == 'delone'){
+	$mi3 = new mysqli('localhost','emg','gullie06','uno_db');
+	if ($mi3->connect_error){
+		echo "noconnect" . mysqli_connect_error() . $mi1->errno;
+	}
+	
+	$th3 = $mi3->thread_id;
+	
+	$id = intval($_POST['delrowid']);
+	
+	if ($_POST['rented'] == 0){
+		$stmt = $mi3->prepare("UPDATE videos SET rented=1 WHERE id=?");
+		$stmt->bind_param("i",$id);
+		
+		$stmt->execute();
+		$stmt->close();
+	}
+	else{
+		$stmt = $mi3->prepare("UPDATE videos SET rented=0 WHERE id=?");
+		$stmt->bind_param("i",$id);
+		
+		$stmt->execute();
+		$stmt->close();
+	}
+	
+	$mi3->kill($th3);
+	$mi3->close();
 }
 
+buildtable();
 
-function buildtable($cats=null, $add=null, $inout=null, $del=null) {
-     $conn = new mysqli('localhost', 'uname', 'pass', 'uno_db');
-    if($conn->connect_error){
-        
+function buildtable() {
+    $mi2 = new mysqli('localhost','emg', 'gullie06','uno_db');
+    if ($mi2->connect_error){
+        echo "noconnect" . mysqli_connect_error() . $mi2->errno;
+        return;
     }
- 
-} 
- 
- 
- 
- 
- 
- /*$name = $_POST['name'];
-$category = $_POST['category'];
-$length = $_POST['length'];
-
-$conn = new mysqli('localhost','emg','gullie06','uno_db');
-
-if ($conn->connect_error)
-{
-	die('error: ' . $conn->connect_error);
+    
+    $th2 = $mi2->thread_id;
+    
+    if (isset($_POST['filter'])) {
+    	if ($_POST['filter'] == 'allmovies'){
+    		$query = 'select * from videos';
+    		$result = $mi2->query($query);
+    	}
+    	else{
+    		$fil = $_POST['filter'];
+    		$qu = "SELECT * FROM videos WHERE category='" . $fil . "'";
+    		$result = $mi2->query($qu);
+    	}
+    }
+    else
+    {
+    	$query = 'select * from videos';
+    	$result = $mi2->query($query);
+    }
+    
+    echo '<table>';
+    while ($row = $result->fetch_assoc()){
+    	echo '<tr id="' . $row['id'] . '"><td>' . $row['id'] . '</td><td>' . $row['vname'] . '</td><td>' . $row['category'] . '</td><td>' . $row['length']; 
+    	if ($row['rented'] == 0){
+    		echo '<td id="crit1">' . '<input type="button" value="Check Out" onclick="delone(' . $row['id'] . ',0)"/></tr>';
+    	}
+    	else {
+    		echo '<td id="crit2">' . '<input type="button" value="Check In" onclick="delone(' . $row['id'] . ',1)"/></tr>';
+    	}
+    }
+    
+    $mi2->kill($th2);
+    $mi2->close();
 }
 
-$th = $conn->thread_id;
-
-$stmt = $conn->prepare("INSERT INTO videos (vname, category, length) VALUES (?, ?, ?)");
-if (!$stmt->bind_param("ssi", $name, $category, $length))
-{
-    echo "bind failed : " . $mysqli->error;    
+echo '<select id="selector" onchange="filter()"><option value="allmovies">All Movies</option>';
+$mysqli = new mysqli('localhost','emg', 'gullie06','uno_db');
+if ($mysqli->connect_error){
+	echo "noconnect" . mysqli_connect_error() . $mysqli->errno;
 }
-else 
-{
-	echo "Bind Succeeded" . "<br>";
+$q = "SELECT DISTINCT category FROM videos";
+$res = $mysqli->query($q);
+
+while ($ent = $res->fetch_assoc()){
+	if (isset($_POST['filter'])){
+		if ($_POST['filter'] == $ent['category']){
+			echo '<option value="' . $ent['category'] . '" selected>' . $ent['category'] . '</option>';
+		}
+		else{
+			echo '<option value="' . $ent['category'] . '">' . $ent['category'] . '</option>';
+		}
+	}
+	else{
+		echo '<option value="' . $ent['category'] . '">' . $ent['category'] . '</option>';
+	}
 }
+	
+echo '</select>';
 
-if (!$stmt->execute())
-{
-    echo "error : " . $mysqli->error;
-}
-else 
-{
-	echo "insertion succeed with values: " . $name . " , " . $category . " , " . $length . "<br>";
-}
+$t = $mysqli->thread_id;
+$mysqli->kill($t);
+$mysqli->close();
 
-$conn->kill($th);
-$conn->close();
-
-*/
-
- 
+?>
